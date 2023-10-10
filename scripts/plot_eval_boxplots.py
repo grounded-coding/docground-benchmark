d28efde0c6440db28d9d5d382580c611293e5cda
nils-hde <nils.hilgers@rwth-aachen.de>
2023-10-10 09:52:42 +0000

    added histogram

--- scripts/plot_eval_boxplots.py
+++ scripts/plot_eval_boxplots.py
@@ -1,23 +1,36 @@
 import json
-import matplotlib.pyplot as plt
-from ..src.data_collector import DataCollector, BEGINDataCollector, DSTCDataCollector
-from ..src.eval_collector import BEGINHumanEvalCollector, DSTCHumanEvalCollector
 
-human_dim = "attributability"
-framework_dim = "groundedness"
+import sys
+sys.path.append('/home/nhilgers/setups/DocGroundEval')
+
+import matplotlib.pyplot as plt
+import numpy as np
+from src.data_collector import DataCollector, BEGINDataCollector, DSTCDataCollector
+from src.eval_collector import BEGINHumanEvalCollector, DSTCHumanEvalCollector
+
+framework_evals_path = "dstc9/test/baseline"
+human_dim = "accuracy"
+framework_dim = "accurate"
+framework_name = "LLEval"
 
 # Replace this with the path to your evaluation file
-evaluation_file = "outputs/tc/dev/baseline/UniEval.json"
+evaluation_file = f"outputs/{framework_evals_path}/{framework_name}.json"
 
-# Replace these with your disjunctive sets of indices
+# BEGIN
 begin = BEGINDataCollector(dataset_path="../BEGIN-dataset/topicalchat", dataset_split="dev", dataset_name="tc")
 sample_indices = begin.get_samples_with_target()
 begin_eval_collector = BEGINHumanEvalCollector(human_eval_path="../BEGIN-dataset/topicalchat/begin_dev_tc.tsv")
 sample_indices, _ = begin_eval_collector.get_subset_with_human_eval(sample_indices, None)
 human_ratings = begin_eval_collector.extract_ratings(sample_indices)
-
-# Using human_ratings we can create 3 disjunctive sets of indices
 index_sets = begin_eval_collector.get_index_sets_disjunctive(sample_indices, human_ratings, human_dim)
+
+# DSTC9
+dstc = DSTCDataCollector(dataset_path="../dstc11-track5/data/dstc9", dataset_split="test", dataset_name="dstc9")
+dstc_eval_coll = DSTCHumanEvalCollector(human_eval_path="../dstc11-track5/results/results_dstc9/baseline/entry0.human_eval.json")
+sample_indices = dstc.get_samples_with_target()
+sample_indices, _ = dstc_eval_coll.get_subset_with_human_eval(sample_indices, None)
+human_ratings = dstc_eval_coll.extract_ratings(sample_indices)
+index_sets = dstc_eval_coll.get_index_sets_disjunctive(sample_indices, human_ratings, human_dim)
 
 # Load the evaluation data
 with open(evaluation_file, "r") as f:
@@ -34,15 +47,29 @@ for index_set in index_sets:
     accur_scores.append(accur_set)
 
 # Create the boxplots
-fig, axes = plt.subplots(1, len(index_sets), sharey=True)
+fig1, axes1 = plt.subplots(1, len(index_sets), sharey=True)
 colors = ['blue', 'green', 'red']
 
-for i, (accur_set, ax, color) in enumerate(zip(accur_scores, axes, colors)):
+for i, (accur_set, ax, color) in enumerate(zip(accur_scores, axes1, colors)):
     ax.boxplot(accur_set, patch_artist=True, boxprops=dict(facecolor=color))
     ax.set_title(f"Index Set {i + 1}")
     ax.set_ylabel("Assigned Score")
     ax.set_xticks([])
 
-# save the plot
-# high dpi
-plt.savefig("outputs/tc/dev/baseline/unieval_boxplot.png", dpi=300)
+# save the boxplot
+plt.savefig(f"outputs/{framework_evals_path}/{framework_name}_boxplot.png", dpi=300)
+plt.clf()
+
+# Create the histograms
+fig2, axes2 = plt.subplots(1, len(index_sets), sharey=True)
+
+for i, (accur_set, ax, color) in enumerate(zip(accur_scores, axes2, colors)):
+    bins = np.linspace(0, max(accur_set) if len(accur_set) > 0 else 1, 6)
+    ax.hist(accur_set, bins=bins, edgecolor='black', color=color, alpha=0.7)
+    ax.set_title(f"Index Set {i + 1}")
+    ax.set_xlabel("Score")
+    if i == 0:  # set y-axis label only for the first subplot
+        ax.set_ylabel("Frequency")
+
+# save the histogram
+plt.savefig(f"outputs/{framework_evals_path}/{framework_name}_histogram.png", dpi=300)
