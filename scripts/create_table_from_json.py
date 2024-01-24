6736c260766a7eefe75d70c539a0c90d4ad749de
nils-hde <nils.hilgers@rwth-aachen.de>
2024-01-24 13:27:18 +0100

    added some scripts and restructured

---
+++ scripts/create_table_from_json.py
@@ -0,0 +1,71 @@
+import json
+import csv
+
+def write_evaluations_to_csv(json_file_path, csv_file_path):
+    # Read the JSON data
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    # Open a CSV file for writing
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+
+        # Write the header row
+        headers = ['response_index', 'accurate', 'appropriate', 'overall', 'accurate_expl', 'appropriate_expl']
+        writer.writerow(headers)
+
+        # Write data rows
+        for entry in data:
+            row=[]
+            for header in headers:
+                cur_entry = entry.get(header, '')
+                if len(header.split('_')) > 1 and header.split('_')[1] == 'expl':
+                    cur_entry = entry.get(header, '')
+                    cur_entry = cur_entry.replace('Explanation: ', '').rstrip("\"").lstrip("\"")
+                row.append(cur_entry)
+            writer.writerow(row)
+
+def write_prediction_to_csv(json_file_path, csv_file_path):
+    # Read the JSON data
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    # Open a CSV file for writing
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+
+        # Write the header row
+        headers = ['response_index', 'response']
+        writer.writerow(headers)
+
+        # Write data rows
+        for index, entry in enumerate(data):
+            if entry.get('target') is True:
+                response = entry.get('response', [''])
+                if isinstance(response, list):
+                    response = response[0]
+                writer.writerow([index, response])
+
+
+# Usage example
+json_file_paths = ['outputs/dstc11/val/llama-7b-ft/geval4_turbo.json',
+                    'outputs/dstc11/val/bart-baseline-ft/geval4_turbo.json',
+                    'outputs/dstc11/val/humanref/geval4_turbo.json',
+                    'outputs/dstc11/train/humanref/geval4_turbo.json'
+                    ]
+
+for json_file_path in json_file_paths:
+    csv_file_path = json_file_path.replace('.json', '.csv')
+
+    write_evaluations_to_csv(json_file_path, csv_file_path)
+
+# Usage example
+pred_json_file_paths = ['../dstc11-track5/pred/val/rg.llama-7b-peft-opt-0104103455.json',
+                        '../dstc11-track5/pred/val/rg.special_tok/bart-base-baseline-0108110257.json',
+                        '../dstc11-track5/data/val/labels.json',
+                        '../dstc11-track5/data/train/labels.json'] 
+
+for pred_json_file_path in pred_json_file_paths:
+    pred_csv_file_path = pred_json_file_path.replace('.json', '.csv')
+
+    write_prediction_to_csv(pred_json_file_path, pred_csv_file_path)
--- scripts/test_metrics_qualitative.py
+++ scripts/test_metrics_qualitative.py
@@ -27,5 +27,27 @@ for candidate_response in candidate_responses:
     print("Knowledge F1: ", kf1.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["knowledge-f1"]))
     # print("LL: ", lleval.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["accurate"]))
     # print("GEval: ", geval.evaluate([candidate_response], gold_responses, turn_historys, context_lists))
-    print("UniEval: ", unieval.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["groundedness"]))
+    # print("UniEval: ", unieval.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["groundedness"]))
+    print("\n")
+
+gold_responses = []
+candidate_responses = []
+context_lists = []
+turn_historys = []
+
+turn_historys.append(["Can you tell me how big the room is I need a lot of space?"])
+context_lists.append(["(THE LENSFIELD HOTEL) The room was a little small for my tastes, and I did feel a little cramped even though I was by myself.", "(THE LENSFIELD HOTEL) The big negative was how small the room was. It did not seem so tiny online. We were pretty cramped during our stay.", "(THE LENSFIELD HOTEL) The spa was not worth the price though, and the room could have been a lot bigger."])
+gold_responses.append("The Lensfield Hotel has small rooms that can feel cramped and could be bigger. Will this fit your needs?")
+
+candidate_responses.append("The rooms at the Lensfield Hotel are pretty small according to most guests. One person said the room was big enough for them though.")
+candidate_responses.append("The rooms at the Lensfield Hotel are small and cramped according to every guest review I found. Do you want to look elsewhere?")
+
+for candidate_response in candidate_responses:
+    print(candidate_response)
+    print("Knowledge BLEU: ", kbleu.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["knowledge-bleu-4"]))
+    print("BLEU: ", bleu.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["bleu-4"]))
+    print("Knowledge F1: ", kf1.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["knowledge-f1"]))
+    # print("LL: ", lleval.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["accurate"]))
+    # print("GEval: ", geval.evaluate([candidate_response], gold_responses, turn_historys, context_lists))
+    # print("UniEval: ", unieval.evaluate([candidate_response], gold_responses, turn_historys, context_lists, dims=["groundedness"]))
     print("\n")
